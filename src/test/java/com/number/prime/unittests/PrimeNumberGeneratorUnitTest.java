8867c1da41d8edbd6dcccaf53c58eb6a2810e705
visheshvashista <35123843+visheshvashista@users.noreply.github.com>
2021-10-01 15:51:57 +0530

    Update PrimeNumberGeneratorUnitTest.java

--- src/test/java/com/number/prime/unittests/PrimeNumberGeneratorUnitTest.java
+++ src/test/java/com/number/prime/unittests/PrimeNumberGeneratorUnitTest.java
@@ -14,12 +14,12 @@ public class PrimeNumberGeneratorUnitTest {
 	@Autowired
     PrimeNumberService primeNumberGenerator;
 	PrimeNumber expectedResult;
-	
+
     @BeforeEach                                         
     public void setUp() throws Exception {
     	primeNumberGenerator = new PrimeNumberService();
     }
-	
+
 	@Test
 	public void getPrimeNumbersUnderZero() throws Exception {
 		Assertions.assertThrows(NoPrimeNumberException.class, () -> {
@@ -32,4 +32,31 @@ public class PrimeNumberGeneratorUnitTest {
 			primeNumberGenerator.generatePrimeNumbers(1L);});
 	}
 
-}
+	@Test
+	public void getPrimeNumbersNegative() throws Exception {
+		Assertions.assertThrows(NoPrimeNumberException.class, () -> {
+			primeNumberGenerator.generatePrimeNumbers(-1001L);});
+	}
+
+	@Test
+	public void getPrimeNumbersUnderEleven() throws Exception {
+		expectedResult = new PrimeNumber("2,3,5,7,11");
+		PrimeNumber primeNumberList = primeNumberGenerator.generatePrimeNumbers(11L);
+		Assertions.assertEquals(expectedResult,primeNumberList);
+	}
+
+	@Test
+	public void getPrimeNumbersUnderHundred() throws Exception {
+		expectedResult = new PrimeNumber("2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97");
+		PrimeNumber primeNumberList = primeNumberGenerator.generatePrimeNumbers(100L);
+		Assertions.assertEquals(expectedResult,primeNumberList);
+	}
+
+	@Test
+	public void getPrimeNumbersUnderTwoHundred() throws Exception {
+		expectedResult = new PrimeNumber("2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59");
+		PrimeNumber primeNumberList = primeNumberGenerator.generatePrimeNumbers(200L);
+		Assertions.assertNotEquals(expectedResult,primeNumberList);
+	}
+
+} 
